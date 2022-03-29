6580dcbf24acfec3c4bc32799c58a3a26ad41ab2
Nahuel Loiacono <nloiacono94@gmail.com>
2022-03-29 12:33:49 -0300

    Version 29-03-22 con Formulario

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('profesores', views.profesores, name="Profesores"),
     path('estudiantes', views.estudiantes, name="Estudiantes"),
     path('entregables', views.entregables, name="Entregables"),
+    path('cursoFormulario', views.cursoFormulario, name="CursoFormulario"),
 ]
 
 
